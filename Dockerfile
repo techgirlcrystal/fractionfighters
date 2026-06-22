d1c9a1f6eed540f24b62bc4681935da1547b2bff
techgirlcrystal <crystal.hale07@gmail.com>
2026-06-22 16:50:48 -0500

    Add PWA support: manifest, service worker, social media cards, icon

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM nginx:alpine
 COPY index.html /usr/share/nginx/html/index.html
+COPY manifest.json /usr/share/nginx/html/manifest.json
+COPY sw.js /usr/share/nginx/html/sw.js
+COPY icon.svg /usr/share/nginx/html/icon.svg
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'fraction-fighters-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(response => {
+        // Cache new requests too
+        if (response && response.status === 200 && e.request.method === 'GET') {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
